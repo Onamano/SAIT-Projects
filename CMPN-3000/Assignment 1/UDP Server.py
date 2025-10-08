cad6dfc579dd67f3194bf61cd87bd36b95fb728f
Mike <129209632+Onamano@users.noreply.github.com>
2025-10-08 12:51:47 -0600

    Working connection and calculation responses

--- CMPN-3000/Assignment 1/UDP Server.py
+++ CMPN-3000/Assignment 1/UDP Server.py
@@ -33,18 +33,21 @@ try:
         if modifiedMessage == "Q":
             reply = "Socket connection closed by user"
             print(reply)
+            serverSocket.sendto(reply.encode(), clientAddress)
             serverSocket.close()
         elif numberFromClient % 2 == 0:
             reply = "Number is even"
             print(reply)
+            serverSocket.sendto(reply.encode(), clientAddress)
         elif numberFromClient % 2 != 0:
             reply = "Number is odd"
             print(reply)
+            serverSocket.sendto(reply.encode(), clientAddress)
         else:
-            print("Socket connection closed")
+            reply = "Socket connection closed by server"
+            print(reply)
+            serverSocket.sendto(reply.encode(), clientAddress)
             serverSocket.close()
-        serverSocket.sendto(reply.encode(), (clientIP, clientPort))
-        modifiedReply, serverAddressUDP = serverSocket.recvfrom(2048)
 except:
     serverSocket.close()
 
@@ -55,3 +58,4 @@ except:
 # References
 # https://sqlpey.com/python/solved-top-5-methods-to-set-timeout-on-pythons-socket-recv-method/ - Setting a timeout for the socket
 # https://www.programiz.com/python-programming/examples/odd-even - number comparisons
+# https://thelinuxcode.com/send_receive_udp_python/ - General troubleshooting help with echoing the calculations
