d9371aee10564dffacd04cb304b76c24baac8e6c
BenGermann <114848211+BenGermann@users.noreply.github.com>
2025-10-12 18:54:36 -0600

    TCP Server.py
    Initial error handling set up, consecutive client inputs can be echoed

--- CMPN-3000/Assignment 1/TCP Server.py
+++ CMPN-3000/Assignment 1/TCP Server.py
@@ -5,26 +5,45 @@ from socket import *
 serverPort = 12000
 serverIP = '0.0.0.0'
 
-# create and bind socket
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind((serverIP,serverPort))
-
-# Set time limit for keeping socket open (20 seconds)
-timeoutSeconds = 20
-serverSocket.settimeout(timeoutSeconds)
-
-# Set port to listen and provide confirmation to user
-serverSocket.listen(1)
-print('The server is ready to receive')
-
-# Receive message and echo it back to the client
+# Create socket, set time limits, and set socket to listen and accept connections
 try:
-    while True:
-        connectionSocket, addr = serverSocket.accept()
-        sentence = connectionSocket.recv(1024).decode()
-        capitalizedSentence = "Echo from Server: " + sentence
-        connectionSocket.send(capitalizedSentence.encode())
-        # connectionSocket.close()
-        print("Received from client: " + sentence)
-except:
-    serverSocket.close()
+    # Create and bind socket, print success and details to terminal
+    serverSocket = socket(AF_INET,SOCK_STREAM)
+    serverSocket.bind((serverIP,serverPort))
+    print('Server socket created and bound at:', serverIP, "and", serverPort)
+
+    # Set time limit for keeping socket open (20 seconds, before user inputs)
+    timeoutSeconds = 20
+    serverSocket.settimeout(timeoutSeconds)
+
+    # Set port to listen and provide confirmation to user
+    serverSocket.listen(1)
+    print('The server is ready to receive')
+
+    # Set socket to accept connections from client
+    connectionSocket, addr = serverSocket.accept()
+
+    # Set time limit for keeping socket open (20 seconds, after user inputs)
+    timeoutSeconds = 20
+    connectionSocket.settimeout(timeoutSeconds)
+
+    # Receive message and echo it back to the client
+    try:
+        while True:
+            sentence = connectionSocket.recv(1024).decode()
+            echoedSentence = "Echo from Server: " + sentence
+            connectionSocket.send(echoedSentence.encode())
+            print("Received from client: " + sentence)
+    except TimeoutError:
+        print("Socket connection has timed out")
+    except:
+        serverSocket.close()
+
+except TimeoutError:
+    print("Socket connection has timed out")
+
+
+# References
+# Base code outline from J. "F. Kurose and K. W. Ross, Computer networking : a top-down approach. Harlow Pearson, 2021."
+# https://stackoverflow.com/questions/42792834/python-continuous-tcp-connection
+# https://labex.io/tutorials/python-how-to-implement-error-handling-in-python-socket-communication-398023
