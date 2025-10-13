d0ab1f2a29a00593b2faddde2fefb2647078858e
BenGermann <114848211+BenGermann@users.noreply.github.com>
2025-10-12 20:57:30 -0600

    Implemented initial error handling, changed input loop logic
    The input loop has been changed to send the server a message indicating the client has disconnected, and entering messages with the length of zero prints to the client that messages must have a length above zero. Currently this stops execution for the client, will likely revisit this to allow for additional inputs after a zero length message.

--- CMPN-3000/Assignment 1/TCP Client.py
+++ CMPN-3000/Assignment 1/TCP Client.py
@@ -2,26 +2,57 @@
 from socket import *
 
 # Connection Variables
-serverName = '192.168.1.78'
+serverName = '192.168.50.38'
 serverPort = 12000
 sentence = ''
 
-# Socket Connection
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-
-# Set time limit for keeping socket open (20 seconds)
-timeoutSeconds = 5
-clientSocket.settimeout(timeoutSeconds)
-
-# Input message & send to server
 try:
-    while sentence != "Q":
-        sentence = input('Input sentence (Q to quit): ')
-        clientSocket.send(sentence.encode())
-        modifiedSentence = clientSocket.recv(1024)
+    # Create client socket
+    clientSocket = socket(AF_INET, SOCK_STREAM)
 
-        # Server Verification Message was Sent and Capitalized
-        print(modifiedSentence.decode())
-except:
+    # Set time limit for keeping socket open (20 seconds, does not currently work due to clientSocket.connect blocking the timeout)
+    timeoutSeconds = 20
+    clientSocket.settimeout(timeoutSeconds)
+
+    try:
+        # Connect to server
+        clientSocket.connect((serverName,serverPort))
+        print('Connected to:', serverName, 'and', serverPort)
+
+        # Input message & send to server
+        try:
+            while True:
+                sentence = input('Input sentence (Q to quit): ')
+                if sentence == 'Q':
+                    sentence = 'Client has disconnected'
+                    clientSocket.send(sentence.encode())
+                    break
+                if len(sentence) == 0:
+                    print('Please enter a message with a length greater than 0')
+                    break
+                clientSocket.send(sentence.encode())
+                echoedSentence = clientSocket.recv(1024)
+
+                # Print the echoed message for the client
+                print(echoedSentence.decode())
+        except TimeoutError:
+            print('Socket connection has timed out')
+        except:
+            clientSocket.close()
+
+    except TimeoutError:
+        print('Socket connection has timed out')
+
+    except ConnectionRefusedError:
+        print('Unable to connect to server')
+
+except OSError:
+    print('Unable to create client socket')
+
+finally:
     clientSocket.close()
+
+# References
+# Base code outline from J. "F. Kurose and K. W. Ross, Computer networking : a top-down approach. Harlow Pearson, 2021."
+# https://stackoverflow.com/questions/42792834/python-continuous-tcp-connection
+# https://labex.io/tutorials/python-how-to-implement-error-handling-in-python-socket-communication-398023
