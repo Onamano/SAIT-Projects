fecb3721975ff690048d7ea1cd8c3041766c3f5a
BenGermann <114848211+BenGermann@users.noreply.github.com>
2025-10-19 14:44:28 -0600

    Restructured into clientSetup function that runs on execution

--- CMPN-3000/Assignment 1/TCP Client.py
+++ CMPN-3000/Assignment 1/TCP Client.py
@@ -1,58 +1,62 @@
 # Imports
 from socket import *
 
-# Connection Variables
-serverName = '192.168.50.38'
-serverPort = 12000
-sentence = ''
-
-try:
-    # Create client socket
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-
-    # Set time limit for keeping socket open (20 seconds, does not currently work due to clientSocket.connect blocking the timeout)
-    timeoutSeconds = 20
-    clientSocket.settimeout(timeoutSeconds)
-
+def clientSetup(serverName, serverPort, sentence):
     try:
-        # Connect to server
-        clientSocket.connect((serverName,serverPort))
-        print('Connected to:', serverName, 'and', serverPort)
+        # Create client socket
+        clientSocket = socket(AF_INET, SOCK_STREAM)
 
-        # Input message & send to server
+        # Set time limit for keeping socket open (20 seconds, does not currently work due to clientSocket.connect blocking the timeout)
+        timeoutSeconds = 20
+        clientSocket.settimeout(timeoutSeconds)
+
         try:
-            while True:
-                sentence = input('Input sentence (Q to quit): ')
-                if sentence == 'Q':
-                    sentence = 'Client has disconnected'
-                    clientSocket.send(sentence.encode())
-                    break
-                if len(sentence) == 0:
-                    print('Please enter a message with a length greater than 0')
-                    break
-                clientSocket.send(sentence.encode())
-                echoedSentence = clientSocket.recv(1024)
+            # Connect to server
+            clientSocket.connect((serverName,serverPort))
+            print('Connected to:', serverName, 'and', serverPort)
 
-                # Print the echoed message for the client
-                print(echoedSentence.decode())
+            # Input message & send to server
+            try:
+                while True:
+                    sentence = input('Input sentence (Q to quit): ')
+                    if sentence == 'Q':
+                        sentence = 'Client has disconnected'
+                        clientSocket.send(sentence.encode())
+                        break
+                    if len(sentence) == 0:
+                        print('Please enter a message with a length greater than 0')
+                        break
+                    clientSocket.send(sentence.encode())
+                    echoedSentence = clientSocket.recv(1024)
+
+                    # Print the echoed message for the client
+                    print(echoedSentence.decode())
+            except TimeoutError:
+                print('Socket connection has timed out')
+            except:
+                clientSocket.close()
+
         except TimeoutError:
             print('Socket connection has timed out')
-        except:
-            clientSocket.close()
 
-    except TimeoutError:
-        print('Socket connection has timed out')
+        except ConnectionRefusedError:
+            print('Unable to connect to server')
 
-    except ConnectionRefusedError:
-        print('Unable to connect to server')
+    except OSError:
+        print('Unable to create client socket')
 
-except OSError:
-    print('Unable to create client socket')
+    #Ensures the socket is closed
+    finally:
+        clientSocket.close()
 
-finally:
-    clientSocket.close()
+if __name__ == "__main__":
+    serverName = '192.168.50.38'
+    serverPort = 12000
+    sentence = ''
+    clientSetup(serverName, serverPort, sentence)
 
 # References
 # Base code outline from J. "F. Kurose and K. W. Ross, Computer networking : a top-down approach. Harlow Pearson, 2021."
 # https://stackoverflow.com/questions/42792834/python-continuous-tcp-connection
 # https://labex.io/tutorials/python-how-to-implement-error-handling-in-python-socket-communication-398023
+# https://realpython.com/python-sockets
