67b698649b6ec2fb1a4cac0d6b2b0650281d2509
Mike <129209632+Onamano@users.noreply.github.com>
2025-10-25 10:56:33 -0600

    Added functions to UDP code

--- CMPN-3000/Assignment 1/UDP Client.py
+++ CMPN-3000/Assignment 1/UDP Client.py
@@ -1,25 +1,28 @@
 # Imports
 from socket import *
 
-# Declare Variables
-serverIP = '192.168.1.78'                                       # IP Address for the UDP server
-serverPort = 12000                                              # Port number for socket connection
-message = ''                                                    # Placeholder for user input variable
+def udpClient (serverIP, serverPort, message):
+    # Create socket
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-# Create socket
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-# Sending data to the server with error handling
-try:
-    while message != "Q":
-        userMessage = input("Enter a number (Q to exit): ")
-        clientSocket.sendto(userMessage.encode(), (serverIP, serverPort))
-        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-        message = userMessage
+    # Sending data to the server with error handling
+    try:
+        while message != "Q":
+            userMessage = input("Enter a number (Q to exit): ")
+            clientSocket.sendto(userMessage.encode(), (serverIP, serverPort))
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+            message = userMessage
         
-        # Receive data from client and print to console, size is 2048 bytes
-        reply, clientAddress = clientSocket.recvfrom(2048)
-        modifiedReply = reply.decode()
-        print("Received from Server: " + modifiedReply)
-except:
-    clientSocket.close()
+            # Receive data from client and print to console, size is 2048 bytes
+            reply, clientAddress = clientSocket.recvfrom(2048)
+            modifiedReply = reply.decode()
+            print("Received from Server: " + modifiedReply)
+    except:
+        clientSocket.close()
+
+if __name__ == "__main__":
+    # Declare Variables
+    serverIP = '192.168.1.78'                                       # IP Address for the UDP server
+    serverPort = 12000                                              # Port number for socket connection
+    message = ''                                                    # Placeholder for user input variable
+    udpClient(serverIP, serverPort, message)
--- CMPN-3000/Assignment 1/UDP Server.py
+++ CMPN-3000/Assignment 1/UDP Server.py
@@ -1,57 +1,58 @@
 # Imports
 from socket import *
 
-# Declare Variables
-clientPort = 12000
-clientIP = '192.168.1.82'
-serverIP = '0.0.0.0'
+def udpServer (clientIP, clientPort):
+    # Create UDP Socket and bind to port 12000
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind((serverIP, clientPort))
 
-# Create UDP Socket and bind to port 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind((serverIP, clientPort))
+    # Set time limit for keeping socket open (20 seconds)
+    timeoutSeconds = 20
+    serverSocket.settimeout(timeoutSeconds)
 
-# Set time limit for keeping socket open (20 seconds)
-timeoutSeconds = 20
-serverSocket.settimeout(timeoutSeconds)
+    # Confirm server is listening
+    print('The Server is ready to receive')
 
-# Confirm server is listening
-print('The Server is ready to receive')
-
-try:
-    while True:
-        # Receive data from client and print to console, size is 2048 bytes
-        message, clientAddress = serverSocket.recvfrom(2048)
-        modifiedMessage = message.decode()
-        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-        print("Received from Client: " + modifiedMessage)
+    try:
+        while True:
+            # Receive data from client and print to console, size is 2048 bytes
+            message, clientAddress = serverSocket.recvfrom(2048)
+            modifiedMessage = message.decode()
+            serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+            print("Received from Client: " + modifiedMessage)
         
-        # Convert number from string to integer
-        if modifiedMessage != "Q":
-            numberFromClient = modifiedMessage
-            numberFromClient = int(numberFromClient)
+            # Convert number from string to integer
+            if modifiedMessage != "Q":
+                numberFromClient = modifiedMessage
+                numberFromClient = int(numberFromClient)
 
-        # Calculations
-        if modifiedMessage == "Q":
-            reply = "Socket connection closed by user"
-            print(reply)
-            serverSocket.sendto(reply.encode(), clientAddress)
-            serverSocket.close()
-        elif numberFromClient % 2 == 0:
-            reply = "Number is even"
-            print(reply)
-            serverSocket.sendto(reply.encode(), clientAddress)
-        elif numberFromClient % 2 != 0:
-            reply = "Number is odd"
-            print(reply)
-            serverSocket.sendto(reply.encode(), clientAddress)
-        else:
-            reply = "Socket connection closed by server"
-            print(reply)
-            serverSocket.sendto(reply.encode(), clientAddress)
-            serverSocket.close()
-except:
-    serverSocket.close()
+            # Calculations
+            if modifiedMessage == "Q":
+                reply = "Socket connection closed by user"
+                print(reply)
+                serverSocket.sendto(reply.encode(), clientAddress)
+                serverSocket.close()
+            elif numberFromClient % 2 == 0:
+                reply = "Number is even"
+                print(reply)
+                serverSocket.sendto(reply.encode(), clientAddress)
+            elif numberFromClient % 2 != 0:
+                reply = "Number is odd"
+                print(reply)
+                serverSocket.sendto(reply.encode(), clientAddress)
+            else:
+                reply = "Socket connection closed by server"
+                print(reply)
+                serverSocket.sendto(reply.encode(), clientAddress)
+                serverSocket.close()
+    except:
+        serverSocket.close()
 
+if __name__ == "__main__":
+    clientPort = 12000
+    clientIP = '192.168.1.82'
+    serverIP = '0.0.0.0'
+    udpServer(clientIP, clientPort)
 
 
 
