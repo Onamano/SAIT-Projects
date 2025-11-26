7cd02804317b0c69b6ea35f74c78eed0f2f507a4
BenGermann <114848211+BenGermann@users.noreply.github.com>
2025-11-25 18:40:00 -0700

    Added comments for what is being tested

--- CMPS-3000/Assignment 3/IntegrationTest.py
+++ CMPS-3000/Assignment 3/IntegrationTest.py
@@ -2,23 +2,23 @@ import unittest
 from Buggy_Code_fixed import Order
 
 class TestIntegration(unittest.TestCase):
-
     #Tests that the functions add_item, remove_item, calculate_total, and apply_discount functions are working together.
     def test_order_integration(self):
+        #Verifies that the value returned for the order total is correct after the order list has had items added and removed.
         order = Order("Alice", [("Book", 2, 15.00), ("Pen", 5, 1.50)])
         order.add_item("Notebook", 3, 5.00)
         order.remove_item("Pen")
         total = order.calculate_total()
         self.assertEqual(total, 45.00)
         
+        #Verifies all discount codes return the correct value.
         discount = order.apply_discount(code="SAVE10")
         self.assertEqual(discount, 0.1)
-
         discount = order.apply_discount(code="SAVE20")
         self.assertEqual(discount, 0.2)
-    
         discount = order.apply_discount(code="SAVE30")
         self.assertEqual(discount, 0.3)
 
 if __name__ == "__main__":
     unittest.main()
+
